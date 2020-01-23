7985db9d0399b9ca033d4ebc8d4585d9613f1f96
Daniel Kidon <dandankidon@gmail.com>
2020-01-23 14:46:12 +0200

    feat: handles all edge cases well

--- listener.js
+++ listener.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
 const { MONGODB_NAME, MONGODB_URL } = require("./config/config");
-const { createGroup, setRoomMembers, closeGroup } = require("./chat");
+const { createGroup, setRoomMembers, closeGroup, renameGroup, sendMessageToGroup } = require("./chat");
 
 dotenv.config();
 
@@ -11,9 +11,10 @@ MongoClient.connect(MONGODB_URL).then(async client => {
     const boardsCursor = boards.watch();
 
     const handleInsertEvent = async (fullDocument) => {
-        const { title, users, _id } = fullDocument;
+        const { users, _id, title } = fullDocument;
         const members = users.map(user => user.id);
-        const roomId = await createGroup(title, members);
+        const roomId = await createGroup(`${title}-${_id}`, members);
+        sendMessageToGroup(roomId, `${GREETING_MSG} - ${title}`);
         boards.updateOne({ _id }, { $set: { chatRoomId: roomId } }, (err, result) => {
             err ? console.error(err) : null;
         });
@@ -23,7 +24,12 @@ MongoClient.connect(MONGODB_URL).then(async client => {
         await closeGroup(roomId);
     };
 
-    const handleUpdateEvent = async (fullDocument, updateDescription) => {
+    const getFullDocumentFromBoardsCollection = async (id) => {
+        return await boards.findOne({ _id: id });
+    }
+
+    const handleUpdateEvent = async (documentId, updateDescription) => {
+        const fullDocument = await getFullDocumentFromBoardsCollection(documentId);
         const { updatedFields } = updateDescription;
         const { chatRoomId } = fullDocument;
 
@@ -34,6 +40,12 @@ MongoClient.connect(MONGODB_URL).then(async client => {
             return await handleInsertEvent(fullDocument);
         }
 
+
+        if (updatedFields && updatedFields["title"]) {
+            const title = updatedFields["title"];
+            await renameGroup(chatRoomId, `${title}-${documentId}`);
+        }
+
         if (updatedFields && updatedFields["users"]) {
             const members = updatedFields["users"].map(({ id }) => id);
             return await setRoomMembers(chatRoomId, members);
@@ -42,12 +54,12 @@ MongoClient.connect(MONGODB_URL).then(async client => {
 
     const handleBoardChange = async document => {
         if (document) {
-            const { fullDocument, operationType, updateDescription } = document;
+            const { fullDocument, documentKey, operationType, updateDescription } = document;
             if (operationType === 'insert') {
                 return await handleInsertEvent(fullDocument);
             }
             if (operationType === 'update') {
-                return await handleUpdateEvent(fullDocument, updateDescription);
+                return await handleUpdateEvent(documentKey._id, updateDescription);
             }
         }
     }
--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
+
 const config = {
     chatUrl: process.env.chatUrl || 'http://localhost:8080',
     chatGroupUrl: process.env.chatGroupUrl || 'group',
     chatLoginUrl: process.env.chatLoginUrl || 'login',
+    chatMessageUrl: process.env.chatMessageUrl || 'chat',
     loginUser: process.env.loginUser || 'user',
     loginPass: process.env.loginPass || 'pass',
     MONGODB_NAME: process.env.MONGODB_NAME || 'kanban',
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://admin:kanban123@cluster0-tk8aq.mongodb.net/'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://admin:kanban123@cluster0-tk8aq.mongodb.net/',
+    GREETING_MSG: process.env.GREETING_MSG || 'This a Group Chat for your new Board on AmanBoard'
 };
 
 module.exports = config;
